af50e8044656254a2fa8330faa25e3469f3fe469
manuel <meth@in.tum.de>
2022-07-13 17:06:12 +0200

    changed answer to name queries

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
+           return "My name is Manuel.";
         } else { // TODO extend the programm here
             if (query.contains("food")) {
                 return "Pizza is hand down the best food ever. Thanks Italy!";
